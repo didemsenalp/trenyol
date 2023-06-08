e1aafe455742e80a874d5546ba1e57b1e46db975
Didem Şenalp <didemsenalp8@gmail.com>
2023-06-08 18:02:26 +0300

    kaybolmasın

--- api.py
+++ api.py
@@ -42,6 +42,7 @@ def check_token(token):
     else:
         return get_anka_result("token kayitli",False,token)
     
+  
 
 @app.route("/UyeOlApi",methods = ["POST"])
 def uye_ol_api():
@@ -62,16 +63,33 @@ def uye_ol_api():
                 
                 if uye_ol(register_name,register_email,register_username,register_password,token,gu_id)["success"] == True:
 
-                    return get_anka_result("Kullanici kaydedildi",True,token)
+                    return uye_ol(register_name,register_email,register_username,register_password,token,gu_id)
                 else:
-                    return get_anka_result("Kullanici kaydedilemedi",False,None)
+                    return uye_ol(register_name,register_email,register_username,register_password,token,gu_id)
             else:
-                return get_anka_result("token kayitli",False,token)
+                return check_token(token)
         else:
-            return get_anka_result("eposta adresi sisteme kayitli.",True,register_email)
+            return check_email(register_email)
     else:
         return validate_uye_ol(register_name,register_email,register_username,register_password)
-    
+
+def validate_uye_ol(name,email,username,password):
+    request_data = request.get_json()
+    name = request_data["name"]
+    email = request_data["email"]
+    username = request_data["username"]
+    password = request_data["password"]
+
+    if name == None or name == "" or type(name) == int:
+        return get_anka_result("isim bos",False,None)
+    if email == None or email == "" or type(email) == int:
+        return get_anka_result("eposta adresi bos",False,None)
+    if username == None or username == "" or type(username) == int:
+        return get_anka_result("username bos",False,None)
+    if password == None or password == "":
+        return get_anka_result("parola bos",False,None)
+    return get_anka_result("Tüm degerler dogru girildi",True,None)
+
 def uye_ol(register_name,register_email,register_username,register_password,token,gu_id):
     uye_ol_query = "Insert into users(name,email,username,password,token,musteri_id) VALUES(%s,%s,%s,%s,%s,%s)"
     
@@ -96,22 +114,6 @@ def check_email(register_email):
     else:
         return get_anka_result("eposta adresi sisteme kayitli.",False,register_email)
 
-def validate_uye_ol(name,email,username,password):
-    request_data = request.get_json()
-    name = request_data["name"]
-    email = request_data["email"]
-    username = request_data["username"]
-    password = request_data["password"]
-
-    if name == None or name == "":
-        return get_anka_result("isim bos",False,None)
-    if email == None or email == "":
-        return get_anka_result("eposta adresi bos",False,None)
-    if username == None or username == "":
-        return get_anka_result("username bos",False,None)
-    if password == None or password == "":
-        return get_anka_result("parola bos",False,None)
-    return get_anka_result("Tüm degerler dogru girildi",True,None)
 
 @app.route("/GirisYapApi",methods = ["POST"])
 def giris_yap_api():
@@ -121,11 +123,19 @@ def giris_yap_api():
 
     if validate_giris_yap(entered_email,entered_password)["success"] == True:
         if check_email_and_password(entered_email,entered_password)["success"] == True:
-            return get_anka_result('Basariyla giris yapildi.',True,None)
+            return check_email_and_password(entered_email,entered_password)
         else:
-            return get_anka_result('Giris yapilamadi.',False,None)
+            return check_email_and_password(entered_email,entered_password)
     else:
         return validate_giris_yap(entered_email,entered_password)
+    
+def validate_giris_yap(email,password):
+
+    if email == None or email == "":
+        return get_anka_result("Eposta adresi bos",False,None)
+    if password == None or password == "":
+        return get_anka_result("Parola bos",False,None)
+    return get_anka_result("Degerler dogru",True,None)
     
 def get_musterid_with_by_token(token):
     query_token = "Select * From users where token = %s"
@@ -155,14 +165,6 @@ def check_email_and_password(email,password):
     else:
          
         return get_anka_result('Eposta adresi kayitli degil',False,None)
-    
-def validate_giris_yap(email,password):
-
-    if email == None or email == "":
-        return get_anka_result("Eposta adresi bos",False,None)
-    if password == None or password == "":
-        return get_anka_result("Parola bos",False,None)
-    return get_anka_result("Degerler dogru",True,None)
 
 @app.route("/KatalogUrunleriGoruntuleApi",methods = ["GET"])
 def katalog_urunleri_goruntule():
@@ -184,33 +186,48 @@ def sepete_urun_ekle():
     token = request.headers.get('token')
     request_data = request.get_json()
     product_id = request_data["product_id"]
-    if get_musterid_with_by_token(token)["success"] == True:
-        musteri_id = get_musterid_with_by_token(token)["data"]
-        if urun_varmi(product_id)["success"] == True:
-            if musterinin_sepeti_var_mi(musteri_id)["success"] == True:
-                musteri_sepet_id= musterinin_sepeti_var_mi(musteri_id)["data"]
-                sepete_urun_ekle_query = "Insert into cart_item(musteri_id,product_id) VALUES(%s,%s)"
-                sepete_urun_ekle_result = g.cursor.execute(sepete_urun_ekle_query,(musteri_id,product_id))
-                if sepete_urun_ekle_result >0:
-                    mysql.connection.commit()
-                    return get_anka_result('Urun sepete eklendi',True,musteri_sepet_id)
+    if validate_token(token)["success"] == True:
+        if get_musterid_with_by_token(token)["success"] == True:
+            musteri_id = get_musterid_with_by_token(token)["data"]
+            if validate_product_id(product_id)["success"] == True:
+                if urun_varmi(product_id)["success"] == True:
+                    if musterinin_sepeti_var_mi(musteri_id)["success"] == True:
+                        musteri_sepet_id= musterinin_sepeti_var_mi(musteri_id)["data"]
+                        sepete_urun_ekle_query = "Insert into cart_item(musteri_id,product_id,cart_id) VALUES(%s,%s,%s)"
+                        sepete_urun_ekle_result = g.cursor.execute(sepete_urun_ekle_query,(musteri_id,product_id,musteri_sepet_id))
+                        if sepete_urun_ekle_result >0:
+                            mysql.connection.commit()
+                            return get_anka_result('Urun sepete eklendi',True,musteri_sepet_id)
+                        else:
+                            return get_anka_result('Urun sepete eklenemedi',False,None)
+                    else:
+                        musteri_sepet_id = sepet_olustur(musteri_id)["data"]
+                        sepete_urun_ekle_query = "Insert into cart_item(musteri_id,product_id) VALUES(%s,%s)"
+                        sepete_urun_ekle_result = g.cursor.execute(sepete_urun_ekle_query,(musteri_id,product_id))
+                        if sepete_urun_ekle_result >0:
+                            mysql.connection.commit()
+                            return get_anka_result('Urun sepete eklendi',True,musteri_sepet_id)
+                        else:
+                            return get_anka_result('Urun sepete eklenemedi',False,None)
                 else:
-                    return get_anka_result('Urun sepete eklenemedi',False,None)
+                    return urun_varmi(product_id)
             else:
-                musteri_sepet_id = sepet_olustur(musteri_id)["data"]
-                sepete_urun_ekle_query = "Insert into cart_item(musteri_id,product_id) VALUES(%s,%s)"
-                sepete_urun_ekle_result = g.cursor.execute(sepete_urun_ekle_query,(musteri_id,product_id))
-                if sepete_urun_ekle_result >0:
-                    mysql.connection.commit()
-                    return get_anka_result('Urun sepete eklendi',True,musteri_sepet_id)
-                else:
-                    return get_anka_result('Urun sepete eklenemedi',False,None)
+                return validate_product_id(product_id)
         else:
-            return get_anka_result('Urun yok',False,None)
+            return get_musterid_with_by_token(token)
     else:
-            return get_anka_result('Kullanici yok',False,None)
-            
-       
+        return validate_token(token)
+    
+def validate_token(token):
+    if token == None or token == "":
+        return get_anka_result("token degeri yanlis",False,None)
+    return get_anka_result("token degeri dogru",True,None)   
+
+def validate_product_id(product_id):
+    if product_id == None or product_id == "" or type(product_id) == str:
+        return get_anka_result('Urun id yanlis',False,None)
+    return get_anka_result('Urun id dogru',True,product_id)
+
 def urun_varmi(product_id):
 
     urun_varmi_query = "Select * From products where product_id = %s"
@@ -222,8 +239,6 @@ def urun_varmi(product_id):
     else:
         
         return get_anka_result('Urun yok.',False,None)
-
-
 
 def musterinin_sepeti_var_mi(musteri_id):
     query_musterinin_sepeti_varmi = "Select * From cart where musteri_id = %s "
@@ -256,5 +271,81 @@ def sepet_olustur(musteri_id):
         return get_anka_result('Musteriye sepet olusturuldu',True,musteri_sepet_id)
     else: 
         return get_anka_result('Musteriye sepet olusturulamadi',False,None)
+
+@app.route("/SepetiGoruntuleApi",methods = ["GET"])
+def sepeti_goruntule():
+    token = request.headers.get('token')
+    
+    if validate_token(token)["success"] == True:
+        if get_musterid_with_by_token(token)["success"] == True:
+            musteri_id = get_musterid_with_by_token(token)["data"]
+            if musterinin_sepeti_var_mi(musteri_id)["success"] == True:
+                musteri_sepet_id = musterinin_sepeti_var_mi(musteri_id)["data"]
+                if sepetteki_urunleri_getir(musteri_sepet_id)["success"] == True:
+                    sepetteki_urunler_listesi = list(sepetteki_urunleri_getir(musteri_sepet_id)["data"])
+                    sepet_tutari = sepet_tutari_hesapla(musteri_sepet_id)["data"]
+                    return get_anka_result("Sepet tutari hesaplandi.",True,sepetteki_urunler_listesi)
+                else:
+                    return sepetteki_urunleri_getir(musteri_sepet_id)
+            else:
+                return musterinin_sepeti_var_mi(musteri_id)
+        else:
+            return get_musterid_with_by_token(token)
+    else:
+        return validate_token(token)
+
+def sepetteki_urunleri_getir(sepet_id):
+    query_sepetteki_urunleri_getir = "Select * From cart_item where cart_id = %s"
+
+    sepetteki_urunleri_getir_result = g.cursor.execute(query_sepetteki_urunleri_getir,(sepet_id,))
+
+    cart_item = g.cursor.fetchall()
+
+    if sepetteki_urunleri_getir_result > 0:
+
+        return get_anka_result('Sepetteki urunler goruntulendi',True,cart_item)
         
+    else:
+        
+        return get_anka_result('Sepet bulunamadi',False,None)
+    
+def sepet_tutari_hesapla(sepet_id):
+    query_musteri_sepeti = "Select * From cart_item where cart_id = %s"
+
+    result_musteri_sepeti = g.cursor.execute(query_musteri_sepeti,(sepet_id,))
+
+    if result_musteri_sepeti > 0 :
+
+        products_id_list = [item['product_id'] for item in g.cursor.fetchall()]
+
+        sepet_tutari = 0
+
+        for product_id in products_id_list :
+
+            get_product_price_metodu = get_product_price(product_id)
+
+            product_price = get_product_price_metodu["product_price"]
+
+            sepet_tutari += product_price
+        
+
+        return get_anka_result("Sepet tutari hesaplandi.",True,sepet_tutari)
+    else:
+        return get_anka_result("Sepet tutari hesaplanamadi.",False,None)
+
+def get_product_price(product_id):
+    query_get_product_price = "Select * From products where product_id = %s"
+
+    result_get_product_price = g.cursor.execute(query_get_product_price,(product_id,))
+
+    product_data = g.cursor.fetchone()
+
+    product_price = product_data["product_price"]
+
+    if result_get_product_price > 0 :
+        return get_anka_result("Urun fiyati bulundu",True,product_price)
+    
+    return get_anka_result("Urun fiyati bulunamadi",False,None)
+
+
 app.run(debug=True)
