72cf28168bdec439a548e3d634378cdec509b175
Didem Şenalp <didemsenalp8@gmail.com>
2023-06-08 15:37:02 +0300

    validate giris yap metodu güncellendi

--- api.py
+++ api.py
@@ -29,6 +29,104 @@ def token_olustur():
    token = str(random.randint(1,100))
    return token
 
+def gu_id_olustur():
+    gu_id = uuid.uuid4()
+    return gu_id
+
+def check_token(token):
+    check_user_with_token_query = "Select * From users where token = %s"
+    check_user_with_token_result = g.cursor.execute(check_user_with_token_query,(token,))
+    if check_user_with_token_result == 0:
+        return get_anka_result("token kayitli degil",True,None)
+        
+    else:
+        return get_anka_result("token kayitli",False,token)
+    
+
+@app.route("/UyeOlApi",methods = ["POST"])
+def uye_ol_api():
+    request_data = request.get_json()
+    register_name = request_data["name"]
+    register_email = request_data["email"]
+    register_username = request_data["username"]
+    register_password = request_data["password"]
+
+    if validate_uye_ol(register_name,register_email,register_username,register_password)["success"] == True:
+        if check_email(register_email)["success"] == True:
+
+            token = token_olustur()
+
+            if check_token(token)["success"] == True:
+
+                gu_id = gu_id_olustur()
+                
+                if uye_ol(register_name,register_email,register_username,register_password,token,gu_id)["success"] == True:
+
+                    return get_anka_result("Kullanici kaydedildi",True,token)
+                else:
+                    return get_anka_result("Kullanici kaydedilemedi",False,None)
+            else:
+                return get_anka_result("token kayitli",False,token)
+        else:
+            return get_anka_result("eposta adresi sisteme kayitli.",True,register_email)
+    else:
+        return validate_uye_ol(register_name,register_email,register_username,register_password)
+    
+def uye_ol(register_name,register_email,register_username,register_password,token,gu_id):
+    uye_ol_query = "Insert into users(name,email,username,password,token,musteri_id) VALUES(%s,%s,%s,%s,%s,%s)"
+    
+    uye_ol_result = g.cursor.execute(uye_ol_query,(register_name,register_email,register_username,register_password,token,gu_id))
+
+    if uye_ol_result > 0:
+
+        mysql.connection.commit()
+
+        return get_anka_result("Kullanici kaydedildi",True,token)
+    else:
+        return get_anka_result("Kullanici kaydedilemedi",False,None)
+    
+def check_email(register_email):
+    check_user_with_email_query = "Select * From users where email = %s"
+
+    check_user_with_email_result = g.cursor.execute(check_user_with_email_query,(register_email,))
+
+    if check_user_with_email_result == 0 :
+        return get_anka_result("eposta adresi kayitli degil",True,None)
+        
+    else:
+        return get_anka_result("eposta adresi sisteme kayitli.",False,register_email)
+
+def validate_uye_ol(name,email,username,password):
+    request_data = request.get_json()
+    name = request_data["name"]
+    email = request_data["email"]
+    username = request_data["username"]
+    password = request_data["password"]
+
+    if name == None or name == "":
+        return get_anka_result("isim bos",False,None)
+    if email == None or email == "":
+        return get_anka_result("eposta adresi bos",False,None)
+    if username == None or username == "":
+        return get_anka_result("username bos",False,None)
+    if password == None or password == "":
+        return get_anka_result("parola bos",False,None)
+    return get_anka_result("Tüm degerler dogru girildi",True,None)
+
+@app.route("/GirisYapApi",methods = ["POST"])
+def giris_yap_api():
+    request_data = request.get_json()
+    entered_email = request_data["email"]
+    entered_password = request_data["password"]
+
+    if validate_giris_yap(entered_email,entered_password)["success"] == True:
+        if check_email_and_password(entered_email,entered_password)["success"] == True:
+            return get_anka_result('Basariyla giris yapildi.',True,None)
+        else:
+            return get_anka_result('Giris yapilamadi.',False,None)
+    else:
+        return validate_giris_yap(entered_email,entered_password)
+    
 def get_musterid_with_by_token(token):
     query_token = "Select * From users where token = %s"
     token_query_result = g.cursor.execute(query_token,(token,))
@@ -40,73 +138,12 @@ def get_musterid_with_by_token(token):
     else:
         return get_anka_result('Boyle bir kullanici bulunamadi.',False,None)
 
-@app.route("/UyeOlApi",methods = ["POST"])
-def uye_ol_api():
-    request_data = request.get_json()
-    register_name = request_data["name"]
-    register_email = request_data["email"]
-    register_username = request_data["username"]
-    register_password = request_data["password"]
+def check_email_and_password(email,password):
+    check_email_query = "Select * From users where email = %s"
 
+    check_email_result = g.cursor.execute(check_email_query,(email,))
 
-    if validation_uye_ol(register_email)["data"] == None:
-
-        token = token_olustur()
-
-        if validation_token(token)["data"] == None:
-
-            gu_id = uuid.uuid4()
-
-            print(gu_id)
-
-            uye_ol_query = "Insert into users(name,email,username,password,token,musteri_id) VALUES(%s,%s,%s,%s,%s,%s)"
-            g.cursor.execute(uye_ol_query,(register_name,register_email,register_username,register_password,token,gu_id))
-                
-            mysql.connection.commit()
-
-            return get_anka_result("Kullanici kaydedildi",True,token)
-        else:
-            return get_anka_result("token kayitli",False,token)
-    else:
-       return get_anka_result("eposta adresi sisteme kayitli.",True,register_email)
-
-def validation_token(token):
-    find_user_with_token_query = "Select * From users where token = %s"
-    find_user_with_token_result = g.cursor.execute(find_user_with_token_query,(token,))
-    if find_user_with_token_result == 0:
-        return get_anka_result("token kayitli degil",True,None)
-        
-    else:
-        return get_anka_result("token kayitli",False,token)
-    
-def validation_uye_ol(register_email):
-    find_user_with_email_query = "Select * From users where email = %s"
-
-    find_user_with_email_result = g.cursor.execute(find_user_with_email_query,(register_email,))
-
-    if find_user_with_email_result == 0 :
-        return get_anka_result("eposta adresi kayitli degil",True,None)
-        
-    else:
-        return get_anka_result("eposta adresi sisteme kayitli.",True,register_email)
-
-@app.route("/GirisYapApi",methods = ["POST"])
-def giris_yap_api():
-    request_data = request.get_json()
-    entered_email = request_data["email"]
-    entered_password = request_data["password"]
-
-    if validation_email_and_password(entered_email,entered_password)["success"] == True:
-        return get_anka_result('Basariyla giris yapildi.',True,None)
-    else:
-        return get_anka_result('Giris yapilamadi.',False,None)
-
-def validation_email_and_password(email,password):
-    validation_email_query = "Select * From users where email = %s"
-
-    validation_email_result = g.cursor.execute(validation_email_query,(email,))
-
-    if validation_email_result > 0 :
+    if check_email_result > 0 :
         register_data = g.cursor.fetchone()
         register_user_password = register_data["password"]
         if password == register_user_password:
@@ -119,6 +156,14 @@ def validation_email_and_password(email,password):
          
         return get_anka_result('Eposta adresi kayitli degil',False,None)
     
+def validate_giris_yap(email,password):
+
+    if email == None or email == "":
+        return get_anka_result("Eposta adresi bos",False,None)
+    if password == None or password == "":
+        return get_anka_result("Parola bos",False,None)
+    return get_anka_result("Degerler dogru",True,None)
+
 @app.route("/KatalogUrunleriGoruntuleApi",methods = ["GET"])
 def katalog_urunleri_goruntule():
     
@@ -165,7 +210,7 @@ def sepete_urun_ekle():
     else:
             return get_anka_result('Kullanici yok',False,None)
             
-        
+       
 def urun_varmi(product_id):
 
     urun_varmi_query = "Select * From products where product_id = %s"
